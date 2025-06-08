62c32e614325783a2fc4a7f133687e57897f1c38
fmcglinn <fraser@geekinside.io>
2025-06-08 11:05:38 +1000

    Modified and removed all references to original community-scripts/ProxmoxVE repo, replacing it with fmcglinn/ProxmoxVE-HelperScripts-local for safer usage. This starts the process of making this only run locally after cloning the repo. Removed API calls for statistics.

--- install/argus-install.sh
+++ install/argus-install.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: MickLesk (CanbiZ)
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/fmcglinn/ProxmoxVE-HelperScripts-local/raw/main/LICENSE
 # Source: https://release-argus.io/
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
@@ -61,15 +61,15 @@ service:
       icon_link_to: https://release-argus.io
       web_url: https://github.com/release-argus/Argus/blob/master/CHANGELOG.md
 
-  community-scripts/ProxmoxVE:
+  fmcglinn/ProxmoxVE-HelperScripts-local:
     latest_version:
       type: github
-      url: community-scripts/ProxmoxVE
+      url: fmcglinn/ProxmoxVE-HelperScripts-local
       use_prerelease: false
     dashboard:
-      icon: https://raw.githubusercontent.com/community-scripts/ProxmoxVE/refs/heads/main/misc/images/logo.png
+      icon: https://raw.githubusercontent.com/fmcglinn/ProxmoxVE-HelperScripts-local/refs/heads/main/misc/images/logo.png
       icon_link_to: https://helper-scripts.com/
-      web_url: https://github.com/community-scripts/ProxmoxVE/releases
+      web_url: https://github.com/fmcglinn/ProxmoxVE-HelperScripts-local/releases
 EOF
 echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
 msg_ok "Setup Config"
